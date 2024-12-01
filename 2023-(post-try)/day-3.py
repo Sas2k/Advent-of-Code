a9721f9c2240db652468762d7615f1f52f57486d
Sas2k <sasen.learnings@gmail.com>
2024-12-01 08:17:15 +0530

    AOC: Solved 2023 - Day 3 - Part 1

--- 2023-(post-try)/day-3.py
+++ 2023-(post-try)/day-3.py
@@ -7,20 +7,56 @@ puzzleInput = open("./day-3-input.txt")
 fileLen = sum(1 for _ in open('./day-3-input.txt'))
 #Processing data into a 2d array
 
-data = [0 for l in range(0, len(puzzleInput.readlines()[0])) - 1]
+print("Advent of Code 2023 (Post) - Day 3")
+debug = input("Debug Mode[y/n]:>")
+
+print("Formatting Data.....")
+data = [["." for _ in range(len(puzzleInput.readline()) + 1)]]
+
+puzzleInput.seek(0)
 
 for i in range(0, fileLen):
     curline = puzzleInput.readline()
-    row = [0] + [i for i in curline[:-1]] + [0]
-    # print(row)
+    row = ["."] + [i for i in curline[:-1]] + ["."]
+    if debug == "y":
+        print(row)
     data.append(row)
 
-data.append([0 for l in range(0, len(puzzleInput.readlines()[0]) - 1)])
+puzzleInput.seek(0)
+data.append(["." for _ in range(len(puzzleInput.readline()) + 1)])
+puzzleInput.close()
 
-print(data)
+print("Complete.")
 
-"""
-for y in range(0, len(data)):
-    for x in range(0, len(data[y])):
+print("Part 1 Running....")
+
+num = ""
+puzzleOutput = 0
+hasNeighbours = False
+
+for y in range(1, len(data) - 1):
+    for x in range(1, len(data[y]) - 1):
+        neighbours = [data[y - 1][x - 1], data[y][x - 1], data[y + 1][x - 1],
+                      data[y - 1][x], data[y + 1][x], data[y - 1][x + 1],
+                      data[y][x + 1], data[y + 1][x + 1]] #Every Neighbour for a cell
         if data[y][x].isdigit():
-"""
+            for n in range(0, len(neighbours)): # Yes, I know it sucks ( O(n^3) for worst time case....)
+                if neighbours[n] != "." and not(neighbours[n].isdigit()):
+                    hasNeighbours = True
+            num += data[y][x]
+            if x == len(data[y]) - 2 and y == len(data) - 2:
+                if hasNeighbours == True:
+                    hasNeighbours = False
+                    puzzleOutput += int(num)
+                    num = ""
+        else:
+            if hasNeighbours == True:
+                hasNeighbours = False
+                puzzleOutput += int(num)
+                num = ""
+            num = ""
+        if debug == "y":
+            print(data[y])
+            print(neighbours, "\n", data[y][x], "\n", hasNeighbours, "\n", puzzleOutput)
+
+print(f"Part 1 Puzzle Output -> {puzzleOutput}")
